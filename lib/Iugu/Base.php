9e2eca93785bf73c8702d76254c63b16600b1229
Patrick Negri <patrick@iugu.com>
2014-03-27 09:18:26 -0300

    [ ! ] Prearing 1.0.5

--- lib/Iugu/Base.php
+++ lib/Iugu/Base.php
@@ -9,7 +9,7 @@ abstract class IuguResource {
 }
 
 abstract class Iugu {
-  const VERSION = "1.0.4";
+  const VERSION = "1.0.5";
 
   public static $api_key = null;
   public static $api_version = "v1";

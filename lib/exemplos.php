7f0107f4e9e9592d17202614f3c9374162be7a1e
Raonny Lourenço <raonny.ruivo@gmail.com>
2017-01-27 15:58:23 -0200

    Alteração no charset e tirando os códigos de teste
    Alteração no charset e tirando os códigos de teste

--- lib/exemplos.php
+++ lib/exemplos.php
@@ -5,7 +5,7 @@
             "agency"                    => '9999-9',
             "account"                   => '999999-9',
             "account_type"              => 'cp',   		// cp, cc
-            "bank"                      => '001',       // "001" para Banco do Brasil, "033" para Santander, "104" para Caixa Econ�mica, "237" para Bradesco, "341" para Ita� e "399" para HSBC
+            "bank"                      => '001',       // "001" para Banco do Brasil, "033" para Santander, "104" para Caixa Econômica, "237" para Bradesco, "341" para Itaú e "399" para HSBC
             "automatic_validation"      => false,
     ));
 
@@ -14,62 +14,62 @@
     //
     // criar sub-conta
     $mktplace = Iugu_Marketplace::create(Array(
-        "action"                    => "create_account", // criei esse atributo action pra ser enviado na api para determinar qual a��o. Achei mais f�cil do que criar novas fun��es
+        "action"                    => "create_account", // criei esse atributo action pra ser enviado na api para determinar qual ação. Achei mais fácil do que criar novas funções
         "name"                      => 'nome',
         "reply_to"                  => 'email@email.com',
     ));
     
-    // verifica��o de subconta
+    // verificação de subconta
     $validate = Iugu_Account::create(Array(
-        "id"                        => '49196DF60BC64B6EB42DEC9C5D81C2CC ',
+        "id"                        => 'CLIENT_ID ',
         "action"                    => "request_verification",
         "automatic_validation"      => false,
         "files"                     => false,
         "data" => array(
             "price_range"           => "Mais que R$ 500,00",
             "physical_products"     => false,
-            "business_type"         => "Faminto - Almo�o Gr�tis",
-            "person_type"           => "Pessoa F�sica",
+            "business_type"         => "TIPO DO CLIENTE",
+            "person_type"           => "Pessoa Física",
             "automatic_transfer"    => false,
             "cpf"                   => '999.999.999-99',
             "name"                  => "nome",
-            "address"               => "endere�o",
+            "address"               => "endereço",
             "cep"                   => "cep",
             "city"                  => "cidade",
             "state"                 => "estado",
             "telephone"             => "27 33333333",
-            "bank"                  => 'Banco do Brasil',       //'Banco do Brasil', 'Ita�', 'Bradesco', 'Caixa Econ�mica', 'Banco do Brasil', 'Santander'
+            "bank"                  => 'Banco do Brasil',       //'Banco do Brasil', 'Itaú', 'Bradesco', 'Caixa Econômica', 'Banco do Brasil', 'Santander'
             "bank_ag"               => '9999-9',
-            "account_type"          => 'Corrente',       // Poupan�a, Corrente
+            "account_type"          => 'Corrente',       // Poupançaa, Corrente
             "bank_cc"               => '999999-9',
         )
     ));
     
     // retornando os dados do mktplace
-    Iugu::setApiKey('1015cd6f9ddc96a357403f994f907cfd');   // user token como parametro
-    $account = Iugu_Account::fetch("49196DF60BC64B6EB42DEC9C5D81C2CC"); // id da conta como parametro
+    Iugu::setApiKey('TOKEN_IUGU');   // user token como parametro
+    $account = Iugu_Account::fetch("CLIENT_ID"); // id da conta como parametro
     
     
     
     // transferendo valor para subconta
     $transfer = Iugu_Transfer::create(Array(
-        "receiver_id"               => "49196DF60BC64B6EB42DEC9C5D81C2CC", // id da conta recebedora
+        "receiver_id"               => "CLIENT_ID", // id da conta recebedora
         "amount_cents"              => '1000' // valor em centavos = R$ 10,00
     ));
     
     
     // retirando valor da subconta
-    Iugu::setApiKey('1015cd6f9ddc96a357403f994f907cfd');   // Chave do user
+    Iugu::setApiKey('TOKEN_IUGU');   // Chave do user
     $transfer = Iugu_Transfer::create(Array(
-        "receiver_id"               => "49196DF60BC64B6EB42DEC9C5D81C2CC", // chave da conta mestre ou da conta recebedora
+        "receiver_id"               => "CLIENT_ID", // chave da conta mestre ou da conta recebedora
         "amount_cents"              => $numValorCentavos
     ));
     
     
     // pedido de retirada de dinheiro
-    Iugu::setApiKey('1015cd6f9ddc96a357403f994f907cfd');   // Chave do user
+    Iugu::setApiKey('TOKEN_IUGU');   // Chave do user
     $withdraw = Iugu_Account::create(Array(
         "action"                    => 'request_withdraw',
-        "account_id"                => '49196DF60BC64B6EB42DEC9C5D81C2CC', // id da conta
+        "account_id"                => 'CLIENT_ID', // id da conta
         "amount"                    => '10.00' // reais com ponto separador decimal R$ 10,00
     ));

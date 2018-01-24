a22be45ded0b1f0fb2b97b0090b83299010f5257
Lucas <just.lucasoliveira@gmail.com>
2018-01-24 17:48:59 -0200

    Change specs to use vcr

--- tests/Iugu/CustomerTest.php
+++ tests/Iugu/CustomerTest.php
@@ -14,17 +14,22 @@ final class Iugu_CustomerTest extends TestCase
 
     public function testShouldCreateACustomer()
     {
+        \VCR\VCR::turnOn();
+        \VCR\VCR::insertCassette('iugu_customer_should_create_a_customer');
 
         $customer = Iugu_Customer::create([
-            "notes" => "Uses emacs",
+            "email" => "martin@fowler.com",
             "name"  => "Martin Fowler",
-            "email" => "martin@fowler.com"
+            "notes" => "Uses emacs"
         ]);
 
         $this->assertEquals($customer->is_new(), false);
-        $this->assertEquals($customer->notes, 'Uses emacs');
-        $this->assertEquals($customer->name,  'Martin Fowler');
         $this->assertEquals($customer->email, 'martin@fowler.com');
+        $this->assertEquals($customer->name,  'Martin Fowler');
+        $this->assertEquals($customer->notes, 'Uses emacs');
+
+        \VCR\VCR::eject();
+        \VCR\VCR::turnOff();
     }
 
 }
